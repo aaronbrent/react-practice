386dddd6b50b8afe77918dadbb9f553348f68bfb
Aaron Ellis <aaronbrentellis@gmail.com>
2017-07-12 19:35:41 -0600

    added an axios get call

--- public/badge.js
+++ public/badge.js
@@ -16,7 +16,7 @@ const Card = (props) => {
 const CardList = (props) => {
 	return (
   	<div>
-      {props.cards.map(card => <Card {...card} />)}
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
     </div>
   );
 }
@@ -25,7 +25,11 @@ class Form extends React.Component {
 	state = { userName: '' }
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Event: Form Submit', this.state.userName);
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp => {
+      	this.props.onSubmit(resp.data);
+				this.setState({ userName: ''});
+      });
   };
 	render() {
   	return (
@@ -41,20 +45,19 @@ class Form extends React.Component {
 }
 
 class App extends React.Component {
-state = {
-	cards: [
-	{ name: "Paul O'Shannessy",
-  	avatar_url: "https://avatars2.githubusercontent.com/u/8445?",
-    company: "Facebook"},
-  { name: "Ben Alpert", 
-  	avatar_url: "https://avatars2.githubusercontent.com/u/6820?",
-    company: "Facebook" },
-	]
-}
+  state = {
+    cards: []
+  };
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+    	cards: prevState.cards.concat(cardInfo)
+    }));
+  };
 	render() {
   	return (
     	<div>
-        <Form />
+        <Form onSubmit={this.addNewCard} />
         <CardList cards={this.state.cards} />
       </div>
     );
