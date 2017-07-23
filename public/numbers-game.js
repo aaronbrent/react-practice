3dc4fd8f27bfb92a1e5738de3590818aa4055f55
Aaron Ellis <aaronbrentellis@gmail.com>
2017-07-23 16:10:52 -0600

    added the ability for user to select and deselect numbers

--- public/numbers-game.js
+++ public/numbers-game.js
@@ -1,16 +1,13 @@
 const Stars = (props) => {
-	const numberOfStars = 1 + Math.floor(Math.random()*9);
   
-  let stars = [];
-  for (let i=0; i<numberOfStars; i++){
-  	stars.push(<i key={i} className='fa fa-star'></i>);
-  }
 	return (
   	<div className='col-5'>
-      {stars}
+      {_.range(props.numberOfStars).map(i => 
+      	<i key={i} className="fa fa-star"></i>
+      )}
     </div>
   );
-}
+};
 
 const Button = (props) => {
 	return (
@@ -23,36 +20,68 @@ const Button = (props) => {
 const Answer = (props) => {
 	return (
   	<div className='col-5'>
-      ...
+      {props.selectedNumbers.map((number, i) =>
+      	<span key={i} onClick={() => props.unselectNumber(number)}>
+          {number}
+        </span>	
+      )}
     </div>
   );
 }
 
 const Numbers = (props) => {
+	const numberClassName = (number) => {
+  	if (props.selectedNumbers.indexOf(number) >= 0) {
+    	return 'selected';
+    }
+  };
 	return (
   	<div className="card text-center">
       <div>
-        <span>1</span>
-        <span className='selected'>2</span>
-        <span className='used'>3</span>
+        {Numbers.list.map((number, i) => 
+        	<span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
+        )}
       </div>
     </div>
   );
-}
+};
+
+Numbers.list = _.range(1, 10);
 
 class Game extends React.Component {
+	state = {
+  	selectedNumbers: [],
+    randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+  };
+  selectNumber = (clickedNumber) => {
+  	if (this.state.selectedNumbers.indexOf(clickedNumber) >=0) { return; }
+    this.setState(prevState => ({
+    	selectedNumbers: prevState.selectedNumber.concat(clickedNumber)
+    }));
+  };
+  unselectNumber = (clickedNumber) => {
+  	this.setState(prevState => ({
+    	selectedNumbers: prevState.selectedNumbers
+      													.filter(number => number !== clickedNumber)
+    }));
+  }
 	render(){
   	return (
     	<div className='container'>
         <h3>Play Nine</h3>
         <hr />
         <div className='row'>
-          <Stars />
+          <Stars numberOfStars={this.state.randomNumberOfStars} />
           <Button />
-          <Answer />
+          <Answer selectedNumbers={this.state.selectedNumbers} 
+                  unselecNumber={this.unselectNumber} />
         </div>
         
-        <Numbers />
+        <Numbers selectedNumbers={this.state.selectedNumbers} 
+                 selectNumber={this.selectNumber} />
       </div>
     );
   }
